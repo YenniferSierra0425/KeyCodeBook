9a80ac5fba286ae9ffe799f975604e82a8e54ab5
YenniferSierra0425 <yennifersierra0425@gmail.com>
2020-10-30 12:31:43 -0500

    Se realizaron cambios en db

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose"); //Paquete que permite la comunicación con nuestra base de datos
+const config = require('./config')
 
 const conectDB = () => {
   //useNewUrlParser: Analizar la información que se requiere enviar a mongoDB.
   //useUnifiedTopology: Escuchar los llamados que hacemos a mongoDB y monitorea que es lo que pasa
-  mongoose.connect('', { useNewUrlParser: true, useUnifiedTopology: true },
+  mongoose.connect(config.mongoDB, { useNewUrlParser: true, useUnifiedTopology: true },
     (error) => {
       if (error) {
         console.log("Error:", error);
